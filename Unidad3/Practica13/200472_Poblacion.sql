dafec1dbff04dd61d914bd7f2e3b409d78803bf4
alejandro <alejandroxlllc@gmail.com>
2024-08-10 21:13:44 -0600

    Corrigiendo errores

--- Unidad3/Practica13/200472_Poblacion.sql
+++ Unidad3/Practica13/200472_Poblacion.sql
@@ -3,10 +3,12 @@ BEGIN
     IF v_password = "xYz$123" THEN
 
         INSERT INTO tbd_horarios  VALUES
-        ('Tiempo completo mañana', 'Lunes', '08:00:00', '16:00:00', 101, 1, 1001,1),
-        ('Tiempo completo tarde', 'Martes', '14:00:00', '22:00:00', 102, 2, 1002,1),
-        ('Tiempo completo noche', 'Miércoles', '22:00:00', '06:00:00', 103, 1, 1003, 0);
-
+        ('Tiempo completo mañana', 'Lunes', '08:00:00', '16:00:00', 1, 1, 1, b'1'),
+        ('Tiempo completo tarde', 'Martes', '14:00:00', '22:00:00', 1, 1, 1, b'1'),
+        ('Medio tiempo noche', 'Miércoles', '22:00:00', '06:00:00', 1, 1, 1, b'0'),
+        ('Medio tiempo mañana', 'Jueves', '08:00:00', '12:00:00', 1, 1, 1, b'1'),
+        ('Medio tiempo tarde', 'Viernes', '14:00:00', '18:00:00', 1, 1, 1, b'1'),
+        ('Descanso', 'Sábado', '00:00:00', '00:00:00', 1, 1, 1, b'1');
     ELSE
         SELECT "La contraseña es incorrecta, no puedo mostrarte el estatus de la Base de Datos" AS ErrorMessage;
     END IF;
