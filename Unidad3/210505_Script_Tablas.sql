78c8ba809dc00299fa382c1c8b95240e68f76ee7
Sebas-ctrl <210505@utxicotepec.edu.mx>
2024-08-12 18:18:10 -0600

    Modificación en el nombre de la tabla de transaccion

--- Unidad3/210505_Script_Tablas.sql
+++ Unidad3/210505_Script_Tablas.sql
@@ -1,11 +1,11 @@
 --
--- Table structure for table `tbb_transacciones`
+-- Table structure for table `tbb_transaccion`
 --
 
-DROP TABLE IF EXISTS `tbb_transacciones`;
+DROP TABLE IF EXISTS `tbb_transaccion`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `tbb_transacciones` (
+CREATE TABLE `tbb_transaccion` (
   `ID` int unsigned NOT NULL AUTO_INCREMENT,
   `Metodo_Pago` enum('Tarjeta de debito','Tarjeta de credito') NOT NULL,
   `Monto` float NOT NULL,
