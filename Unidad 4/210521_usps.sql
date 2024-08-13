2110a5eefe9dd0b15ca9af8d9c1d340fad04c55d
Ame <ame@gmail.com>
2024-08-12 21:16:07 -0600

    functions, usps, poblacion

---
+++ Unidad 4/210521_usps.sql
@@ -0,0 +1,35 @@
+-- Elaborado por: Ameli Pérez Garrido
+-- Grado y Grupo: 9° "B"
+-- Programa Educativo: Ingenieria en Desarrollo y Gestion de Software
+-- Fecha elaboracion: 12 de agosto 2024
+
+CREATE DEFINER=`ame`@`%` PROCEDURE `sp_inserta_transacciones`(v_cantidad INT)
+BEGIN
+	DECLARE i INT DEFAULT 0;
+    DECLARE fecha_transaccion DATETIME;
+    DECLARE fecha_actualizacion DATETIME;
+    DECLARE usuario_id INT;
+    
+	WHILE i < v_cantidad DO
+		SET fecha_transaccion = fn_genera_fechahora('2024-01-01', CURDATE(), '00:00:00', CURTIME());
+        IF DATE(fecha_transaccion) < CURDATE() THEN 
+			SET fecha_actualizacion = fn_genera_fechahora(fecha_transaccion, CURDATE(), '00:00:00', CURTIME());
+		ELSE
+			SET fecha_actualizacion = NULL;
+        END IF;
+        
+        SET usuario_id = (SELECT ID FROM tbb_usuarios ORDER BY RAND() LIMIT 1);
+        
+		INSERT INTO tbb_transacciones VALUES(
+			DEFAULT,
+            fn_numero_aleatorio(1,2),
+            fn_genera_monto_aleatorio(100.0, 2000.0),
+            fn_numero_aleatorio(0,1),
+            fecha_transaccion,
+            fecha_actualizacion,
+            usuario_id
+		);
+        
+		SET i = i + 1;
+    END WHILE;
+END
